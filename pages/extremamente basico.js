ce5323c5c4a0f33a01ebe7a62bedebff3481827c
paulo007.sn <paulo007.sn@gmail.com>
2025-05-08 01:10:01 +0000

    first commit

---
+++ pages/extremamente basico.js
@@ -0,0 +1,12 @@
+function somar(valor1, valor2){
+    return valor1 + valor2;
+}
+
+let teste1 = (10, 9)
+let teste2 = (-10, 4)
+let teste3 = somar(15, -7)
+
+console.log(`
+    X1 = ${teste1}
+    X2 = ${teste2}
+    X3 = ${teste3}`)
